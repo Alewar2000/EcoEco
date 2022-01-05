40d9eb4c6711144821917bfbb97cf9d101aacab2
Alvaro Corcoles <alvarocorco@gmail.com>
2022-01-05 18:07:54 +0100

    Dashboard de Admin
    Panel de control para el admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,23 @@
 @extends('admin.master')
+
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="container-fluid">
+	<div class="panel">
+		<div class="header">
+			<h2 class="title">
+				<i class="fas fa-home">Dashboard</i>
+			</h2>
+
+			
+		</div>
+
+		<div class="inside">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+			</div>
+	</div>
+</div>
+@endsection()
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>@yield('title')</title>
+		<title>@yield('title') - EcoEco</title>
 		<meta name="csrf-token" content="{{csrf_token()}}">
 		<meta name="routeName" content="{{Route::currentRouteName()}}">
 
 
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 		<link rel="stylesheet" href="{{url('/static/css/admin.css?v='.time())}}">
 
@@ -26,7 +26,58 @@
 		
 		<div class="wrapper">
 			<div class="col1">@include('admin.sidebar')</div>
-			<div class="col2"></div>
+			<div class="col2">
+				<nav class="navbar navbar-expand-lg shadow">
+					<div class="collapse navbar-collapse">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a href="{{url('/admin')}}" class="nav-link">
+									<i class="fas fa-home">Dashboard</i>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+				<div class="page">
+
+					<div class="container-fluid">
+						<nav aria-label="breadcrumb shadow">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item">
+									<a href="{{url( '/admin') }}"><i class="fas fa-home">Dashboard</i></a>
+								</li>
+								@section('breadcrumb')
+
+								@show
+							</ol>
+						</nav>
+					</div>
+
+					@if(Session::has('message'))
+					<div class="container">           
+						<div class="alert alert-{{ Session::get('typealert') }} mtop16" style="display:block; margin-bottom: 16px;"> 
+							{{ Session::get('message') }}
+							@if ($errors->any())       
+							<ul>
+								@foreach($errors->all() as $error)
+								<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+							@endif
+							<script>
+								$('.alert').slideDown();
+								setTimeout(function(){ $('.alert').slideUp(); }, 5000);
+							</script>
+						</div>
+					</div>
+					@endif
+
+					@section('content')
+					@show
+
+				</div>
+
+			</div>
 		</div>
 	</body>
 	</html>
